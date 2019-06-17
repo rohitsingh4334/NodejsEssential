ce531ced412687b32cc7352733d91a59011abc88
rohitsingh4334 <rohitsingh4334@gmail.com>
2019-06-17 10:08:30 +0530

    added new chapter

---
+++ ch_09/01/ws.js
@@ -0,0 +1,16 @@
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({port: 3000});
+
+wss.on("connection", function(ws){
+    
+    ws.on("message", function(message){
+        if (message === 'exit'){
+            ws.close();
+        } else {
+            wss.clients.forEach(function (client){
+                client.send(message);
+            });
+        }
+    })
+    ws.send("Welcome to the chat");
+});
